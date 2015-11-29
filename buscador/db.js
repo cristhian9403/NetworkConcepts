09b7d3bb62ba8f71fd84bae8aef640ac66bae661
cristhian9403 <cristhian.camilo.alvarez@correounivalle.edu.co>
2015-11-29 18:04:45 -0500

    buscador

---
+++ buscador/db.js
@@ -0,0 +1,41 @@
+/* XSearch version 5.2 - Database file */
+
+/* configuration settings */
+
+'<meta charset="utf-8">'
+'<meta http-equiv="X-UA-Compatible" content="IE=edge">'
+'<meta name="viewport" content="width=device-width, initial-scale=1">'
+
+	searchname = 'buscar.html'
+	
+	usebannercode=true
+ButtonCode = "<button type='submit' align='center' class='btn btn-primary btn-lg '><span class= 'fa fa-search fa-lg' > Buscar  </span></button>"
+	function templateBody() {
+		document.write('<'+
+		'script language="Javascript">'+
+		'<'+'/'+'script'+'><div > <table border=0 width=100%><tr><td class="span6" align="center">');
+	}
+
+	function templateEnd() {
+		document.write('</td></tr></table></div>');
+	}
+	function bannerCode() {
+	}	
+
+/* end configuration settings */
+
+/* database records */
+
+add("<a href='../vistas/Psicologia/Definicion.html'>Definición de la Psicología</a>","Definicion","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Historia.html'>Historia de la psicología</a>","Fundamentos Históricos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Biologicos.html'>Temas de Biologia</a>","Fundamentos Biológicos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Filosoficos.html'>Filosofia</a>","Fundamentos Filosóficos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Glosario.html'>Glosario</a>","Glosario","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Social.html'>Psicología Social</a>","Social","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Educativa.html'>Psicología Educativa</a>","Educativa","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Clinica.html'>Psicología Clínica</a>","Clínica","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Organizacional.html'>Psicología Organizacional</a>","Organizacional","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Eventos.html'>Eventos</a>","Eventos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Presentacion.html'>Presentación</a>","esta es la pagina de Psicología a tu alcance")
+add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","esta es la pagina de Psicología a tu alcance")
+add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","esta es la pagina de Psicología a tu alcance")
