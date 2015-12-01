333c2c8ec4ddd5f2e07801ce51404d1e362ad5ab
cristhian9403 <cristhian.camilo.alvarez@correounivalle.edu.co>
2015-12-01 00:01:40 -0500

    listo

--- principal.php
+++ principal.php
@@ -97,24 +97,20 @@
 	<div id="player">This text will be replaced</div>
     <div  class="col-md-6" >
       <script type="text/javascript">
-		var so = new SWFObject('mediaplayer/mediaplayer.swf','mpl','420','20','7');
+		var so = new SWFObject('mediaplayer/mediaplayer.swf','mpl','420','40','7');
 		so.addParam('allowfullscreen','true');
-<<<<<<< HEAD
-so.addVariable('backcolor','0xCCCCFF');
-so.addVariable('lightcolor','0x99FFFF');
-so.addVariable('largecontrols','true');
-so.addVariable('autostart','true');
-=======
 		so.addVariable('file','mediaplayer/audios/danzahungara.mp3');
-		so.addVariable('height','20');
-		so.addVariable('width','420');
+		so.addVariable('backcolor','0xCCCCFF');
+		so.addVariable('lightcolor','0x99FFFF');
+		so.addVariable('largecontrols','true');
 		so.addVariable('autostart','true');
->>>>>>> origin/master
 		so.write('player');
 	</script>
         </div>
      </div>
 		<br>
+
+
 		
 		
 		<div STYLE="position:relative; top:-18px; left:55px; width:1100px;">
