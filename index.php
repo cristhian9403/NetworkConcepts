9791a2600c367aee6bcb8bcd27f69a6ce0517c25
marisolde <marisol_d_e@hotmail.com>
2015-12-01 13:31:35 -0500

    vista principal

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
 	<link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/estiloZoom.css" rel="stylesheet" type="text/css">
 	
+	<meta name="keywords" content="VideoLightBox, LightBox, Video Gallery, video html, html video player" />
+		<meta name="description" content="VideoLightBox Gallery created with VideoLightBox, a free wizard program that helps you easily generate beautiful Lightbox-style web video galleries" />
+		<link rel="shortcut icon" href="favicon.ico" />
+		
+		<!-- Start VideoLightBox.com HEAD section -->
+		<link rel="stylesheet" href="index_videolb/videolightbox.css" type="text/css" />
+		
+			<link rel="stylesheet" type="text/css" href="index_videolb/overlay-minimal.css"/>
+			<script src="index_videolb/jquery.js" type="text/javascript"></script>
+			<script src="index_videolb/swfobject.js" type="text/javascript"></script>
+	
+	
 	<title>NetworkConcepts</title>
 	
 	<script>
@@ -63,10 +75,19 @@
 		<div STYLE="position:relative; top:8px; left:55px; width:1000px;">
 						
 					<div class="col-sm-6">
+					
+					<div class="row ">
+						
+							<div class="videogallery col-md-offset-4" >
+								
+							   <a class="voverlay" href="index_videolb/vdbplayer.swf?volume=100&url=video/muere_de_risa_con_las_imagenes_mas_creativas_hechas_con_photoshop.mp4" ><img src="index_videolb/thumbnails/logoemp.jpg"  class="img-rounded" style="border-style:double; border-width:5px;" /><span></span></a><span class="videolb"><a href="http://videolightbox.com">how to insert video in html</a> by VideoLightBox.com v3.1</span>
+							</div>
+												
+						   		
+			            </div>	
+					
 									
-						<div class="col-md-offset-4">	
-							<label class="zoom sobre img-circle" onmouseover="primero.playclip()"><img  src="imagenes/logoemp.jpg" alt="Chania" class="img-rounded" style="border-style:double; border-width:5px;" ></label>						
-						</div>
+						
 					</div>
 								
 					<div class="col-md-6" >
@@ -101,37 +122,10 @@
 					<br>
 		</div>					
 	</div>				
-						
-						
-				
 
-												 
-												
-												
-											
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		<script src="index_videolb/jquery.tools.min.js" type="text/javascript"></script>
+	<script src="index_videolb/videolightbox.js" type="text/javascript"></script>
+	<!-- End VideoLightBox.com BODY section -->
 	
 	</body>
 </html>
